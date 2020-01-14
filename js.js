18e8b69cffd8659e67be0343fb699ff3d762f46c
ma-daria <marina.dasha18@yandex.ru>
2020-01-14 22:47:33 +0300

    color

--- js.js
+++ js.js
@@ -1,5 +1,6 @@
 let canvas = document.querySelector("canvas");
 let context = canvas.getContext("2d");
+let color_id= 'black';
 
 canvas.height = window.innerHeight;
 canvas.width = window.innerWidth;
@@ -24,7 +25,33 @@ document.onclick = function() {
 
 function Draw() {
     context.beginPath();
+    context.strokeStyle = color_id;
     context.quadraticCurveTo(P1[0], P1[1], P2[0], P2[1]);
     context.closePath();
     context.stroke();
 }
+
+
+let FizzyText = function() {
+    this.color_change = 'black';
+
+};
+
+window.onload = function() {
+    var text = new FizzyText();
+    var gui = new dat.GUI();
+    var controllerColorChange = gui.add(text, 'color_change', ['black', 'red', 'green', 'blue' ] );
+
+
+    controllerColorChange.onChange(function(value) {
+        if (value == 'red')
+            color_id = value;
+        if (value == 'green')
+            color_id = value;
+        if (value == 'blue')
+            color_id = value;
+        if (value == 'black')
+            color_id = value;
+    });
+};
+
